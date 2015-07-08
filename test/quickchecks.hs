70a2662f7167348698de861d7a784cb984d93dd6
Coda Hale <coda.hale@gmail.com>
2015-07-08 06:39:20 -0700

    Improve quickchecks.

--- test/quickchecks.hs
+++ test/quickchecks.hs
@@ -1,28 +1,42 @@
 module Main where
 
 import qualified Data.ByteString as B
+import           Data.List ( permutations )
 import qualified Data.Map.Strict as Map
 import           Data.Word
 import           Shamir
 import           Test.QuickCheck
 import           Test.QuickCheck.Monadic
 
-instance Arbitrary B.ByteString where
-    arbitrary =
-        fmap B.pack arbitrary
+genContext :: Gen Context
+genContext = do
+    s <- listOf1 arbitrary
+    n <- choose (3, 255)
+    k <- choose (2, n)
+    i <- choose (0, n - 1)
+    return $
+        Context
+            (B.pack s)
+            (fromIntegral n)
+            (fromIntegral k)
+            (take k $ permutations [1 .. (fromIntegral n)] !! i)
 
-prop_canCombineShares :: B.ByteString -> Positive Word8 -> Positive Word8 -> Property
-prop_canCombineShares s pN pK =
-    (n > 1) ==>  -- have to split into more than one share
-    (k > 1) ==>  -- have to require more than one share to combine
-    (n >= k) ==> -- have to require no more shares to combine than we create
+data Context = Context
+    { secret :: B.ByteString
+    , shares :: Word8
+    , threshold :: Word8
+    , recovered :: [Word8]
+    } deriving (Show)
+
+-- | The results of a split should always be combinable.
+prop_bijective :: Property
+prop_bijective =
+    forAll genContext $ \c ->
     monadicIO $ do
-        shares <- run $ Shamir.split n k s
-        -- TODO: specify which shares via quickcheck inputs
-        assert $ s == (combine . Map.filterWithKey (\v _ -> v <= k)) shares
-  where
-    n = getPositive pN
-    k = getPositive pK
+        s <- run $ Shamir.split (shares c) (threshold c) (secret c)
+        assert $ secret c == (combine . subset c) s
+        where
+          subset c = Map.filterWithKey (\v _ -> v `elem` recovered c)
 
 main :: IO ()
-main = quickCheck prop_canCombineShares
+main = quickCheck prop_bijective
